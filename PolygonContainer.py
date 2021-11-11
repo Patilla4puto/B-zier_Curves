754a98655997cc7c3436f8514f54e0277574c4e0
Tim Meyran <timmeyran@gmail.com>
2021-11-11 15:50:05 +0100

    cleanup & mouse events

---
+++ PolygonContainer.py
@@ -0,0 +1,28 @@
+from matplotlib.path import Path
+import matplotlib.patches as patches
+import numpy as np
+
+class PolygonContainer:
+    def __init__(self, ax, lineWidth = 1):
+        self.lineWidth = lineWidth
+        self.ax = ax
+        self.lines = []
+        self.patches = []
+
+    def clearLines(self):
+        self.lines.clear()
+
+    def addLine(self, line, color='black'):
+        self.lines.append((line, color))
+
+    def redraw(self):
+        for p in self.patches:
+            p.remove()
+        self.patches.clear()
+
+        for line, color in self.lines:
+            #line = np.append(line,line[-1])
+            #line.append(line[-1])  # last line segment has to be duplicated, otherwise it will not be drawn
+            path = Path(line, closed=False)
+            patch = patches.PathPatch(path, facecolor='none', edgecolor=color, lw=self.lineWidth)
+            self.patches.append(self.ax.add_patch(patch))
---
+++ PressablePoint.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+from matplotlib.backend_bases import MouseButton
+
+
+class PressablePoint:
+    def __init__(self, ax, coord, onRemove, onMove, color):
+        self.onRemove = onRemove
+        self.onMove = onMove
+        self.press = False
+        self.moved = False
+        self.circle = plt.Circle(coord, 0.01, color=color)
+        ax.add_patch(self.circle)
+
+    def connect(self):
+        """Connect to all the events we need."""
+        self.cidpress = self.circle.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.cidrelease = self.circle.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        self.cidmotion = self.circle.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        if event.inaxes != self.circle.axes:
+            return
+
+        contains, attrd = self.circle.contains(event)
+        if not contains:
+            return
+
+        if event.button is MouseButton.LEFT:
+            self.press = True
+
+    def on_release(self, event):
+        contains, attrd = self.circle.contains(event)
+        if not contains:
+            return
+
+        if event.button is MouseButton.LEFT and not self.moved:
+            self.onRemove(self)
+            self.circle.remove()
+
+        self.press = False
+        self.moved = False
+
+    def on_motion(self, event):
+        if event.button is MouseButton.LEFT and self.press and event.xdata and event.ydata:
+            print(event.xdata)
+            self.circle.center = (event.xdata, event.ydata)
+            self.circle.figure.canvas.draw()
+            self.onMove(self)
+            self.moved = True
+
+    def get_coordinates(self):
+        return self.circle.center
+
+    def disconnect(self):
+        """Disconnect all callbacks."""
+        self.circle.figure.canvas.mpl_disconnect(self.cidpress)
