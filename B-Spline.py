23bbb38258530ec056bccf046a5286e516b271a1
Juan <ja.gordillo.2018@alumnos.urjc.es>
2021-12-13 11:24:31 +0100

    "b-spline.py created and bezier code added"

---
+++ B-Spline.py
@@ -0,0 +1,189 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import matplotlib
+from Colors import Colors
+import scipy.special as s
+from matplotlib.backend_bases import MouseButton
+
+from PressablePoint import PressablePoint
+from PolygonContainer import PolygonContainer
+
+matplotlib.use('Qt5Agg')
+c = Colors()
+curves = []
+arrow = None
+polinoms = []
+t = .5
+
+fig, ax = plt.subplots(1, 2)
+plt.subplots_adjust(bottom=0.25)
+ax[0].set_xlim(0, 1)
+ax[0].set_ylim(0, 1)
+ax[0].set_aspect('equal', adjustable='box', anchor='C')
+ax[1].set_xlim(0, 1)
+ax[1].set_ylim(0, 1)
+ax[1].set_aspect('equal', adjustable='box', anchor='C')
+
+curveContainer = PolygonContainer(ax[0], 2)
+controlPolygonContainer = PolygonContainer(ax[0])
+bernsteinContainer = PolygonContainer(ax[1])
+
+
+def bernstein_Polynomi(_t, points):
+    n = len(points) - 1
+    polinoms = []
+    for i in range(n + 1):
+        aux = s.binom(n, i) * (_t ** i) * ((1 - _t) ** (n - i))
+        polinoms.append([_t, aux])
+    return polinoms
+
+
+def deCasteljau(t, points):
+    a = [points]
+    print(a)
+    for r in range(1, len(points)):
+        lenght = (len(a[0]) - 1)
+        a.insert(0, [0] * lenght)
+        for j in range(lenght):
+            a[0][j] = ((1 - t) * a[1][j] + t * a[1][j + 1])
+    return a
+
+
+def drawBersteinpolinoms(points):
+    bernsteinContainer.clearLines()
+
+    lines = []
+    for i in range(len(points)):
+        lines.append([])
+        bernsteinContainer.addLine(lines[i], c.colors[i])
+
+    for _t in np.arange(0, 1.02, 0.01):
+        result = bernstein_Polynomi(_t, points)
+        for i in range(len(result)):
+            lines[i].append(result[i])
+
+    bernsteinContainer.redraw()
+
+
+def draw_curve(points):
+    global curves
+    for e in curves:
+        e.remove()
+
+    curves.clear()
+    line = []
+
+    for t in [*np.arange(0, 1.0, 0.1 / len(points)), 1.0]:
+        result = deCasteljau(t, points)[0][0]
+        line.append(result)
+    # curves.append(drawPolygon(lines, ax))
+    curveContainer.clearLines()
+    curveContainer.addLine(line, 'green')
+    curveContainer.redraw()
+
+
+def drawLines(points, t):
+    iterations = deCasteljau(t, points)
+    controlPolygonContainer.clearLines()
+    for i in range(1, len(iterations) + 1):
+        controlPolygonContainer.addLine(iterations[-i], c.getColor())
+
+    vector = (iterations[1][1] - iterations[1][0])*0.2
+    global arrow
+    if arrow != None: arrow.remove()
+
+    arrow = ax[0].arrow(iterations[0][0][0], iterations[0][0][1], vector[0], vector[1], head_width=0.02,zorder=10)
+    controlPolygonContainer.redraw()
+
+
+axTSlider = plt.axes([0.15, 0.1, 0.65, 0.03])
+global t_slider
+t_slider = Slider(
+    ax=axTSlider,
+    label='t',
+    valmin=0.0,
+    valmax=1.0,
+    valinit=t,
+)
+
+controlPoints = []
+controlPointsCoordinates = np.array([])
+
+
+def on_remove(p):
+    p.disconnect()
+    controlPoints.remove(p)
+    global controlPointsCoordinates
+    controlPointsCoordinates = np.array(list(map(lambda p: p.get_coordinates(), controlPoints)))
+    redrawAll()
+
+
+def on_move(o):
+    global controlPointsCoordinates
+    controlPointsCoordinates = np.array(list(map(lambda p: p.get_coordinates(), controlPoints)))
+    redrawAll()
+
+
+def add_control_point(_ax, x, y):
+    circle = PressablePoint(_ax, [x, y], on_remove, on_move, c.colors[len(controlPoints)])
+    circle.connect()
+    controlPoints.append(circle)
+    global controlPointsCoordinates
+    controlPointsCoordinates = np.array(list(map(lambda p: p.get_coordinates(), controlPoints)))
+
+
+add_control_point(ax[0], 0.1, 0.1)
+add_control_point(ax[0], 0.1, 0.8)
+add_control_point(ax[0], 0.8, 0.8)
+add_control_point(ax[0], 0.8, 0.1)
+
+currentPoint = deCasteljau(t, controlPointsCoordinates)[0][0]
+
+circle = plt.Circle(currentPoint, 0.015, color="blue")
+ax[0].add_patch(circle)
+circle_b = []
+
+
+def redraw():
+    c.resetIndex()
+    drawLines(controlPointsCoordinates, t)
+    p = deCasteljau(t, controlPointsCoordinates)[0][0]
+    pts_bernstein = bernstein_Polynomi(t, controlPointsCoordinates)
+    circle.set_center(p)
+    circle.figure.canvas.draw()
+    for i in range(len(circle_b)):
+        circle_b[i].remove()
+    circle_b.clear()
+
+    for i in range(len(pts_bernstein)):
+        circle_b.insert(0, plt.Circle(pts_bernstein[i], 0.01, color=c.colors[i]))
+        ax[1].add_patch(circle_b[0])
+
+
+def redrawAll():
+    redraw()
+    global controlPointsCoordinates
+    draw_curve(controlPointsCoordinates)
+    drawBersteinpolinoms(controlPointsCoordinates)
+    plt.draw()
+    print("redraw all")
+
+
+def updateT(val):
+    global t
+    t = val
+    redraw()
+
+
+def onCanvasClick(event):
+    if event.button is MouseButton.RIGHT and event.xdata and event.ydata:
+        add_control_point(ax[0], event.xdata, event.ydata)
+        redrawAll()
+
+
+fig.canvas.mpl_connect('button_release_event', onCanvasClick)
+
+redrawAll()
+t_slider.on_changed(updateT)
+plt.show()
--- BezierSurface.py
+++ BezierSurface.py
@@ -26,20 +26,19 @@ def deCasteljauCurves(t, points):
 
     for r in range(1, len(points)):
         lenght = (len(a[0]) - 1)
-        a.insert( 0,lenght *[0] )
-
+        a.insert(0, lenght * [0])
 
         for j in range(lenght):
             a[0][j] = ((1 - t) * a[1][j] + t * a[1][j + 1])
 
-    return a[0],a[1]
+    return a[0], a[1]
 
 
 def deCasteljauSurface(s, t, points):
     list = []
     for e in points:
         list.append(deCasteljauCurves(t, e)[0][0])
-        #print(list)
+        # print(list)
     return (deCasteljauCurves(s, list))
 
 
@@ -83,48 +82,50 @@ def drawPoints(axis, points):
 
 def createBezierSurface(points, sSteps, tSteps):
     grid = np.empty((sSteps + 1, tSteps + 1, 3))
-    for j in np.arange(0, 1.0+1/(sSteps), 1 / sSteps):
-        for t in np.arange(0, 1.0 +1 / (tSteps), 1 / tSteps):
-
+    for j in np.arange(0, 1.0 + 1 / (sSteps), 1 / sSteps):
+        for t in np.arange(0, 1.0 + 1 / (tSteps), 1 / tSteps):
             p = deCasteljauSurface(j, t, points)[0]
 
             grid[int(j * (sSteps)), int(t * (tSteps))] = p[0]
 
     return grid
-def drawSurface( grid):
-    for i in range(len(grid)-1):
-        for j in range(len(grid[i])-1):
-            addTriangle([grid[i][j],grid[i+1][j],grid[i][j+1]])
-            addTriangle([grid[i][j+1],grid[i+1][j],grid[i+1][j+1]])
-def drawVectorsPoint(points, s, t,ax):
-    p0,va0= deCasteljauSurface(s, t, points)
-    v0 = va0[1]-va0[0]
+
+
+def drawSurface(grid):
+    for i in range(len(grid) - 1):
+        for j in range(len(grid[i]) - 1):
+            addTriangle([grid[i][j], grid[i + 1][j], grid[i][j + 1]])
+            addTriangle([grid[i][j + 1], grid[i + 1][j], grid[i + 1][j + 1]])
+
+
+def drawVectorsPoint(points, s, t, ax):
+    p0, va0 = deCasteljauSurface(s, t, points)
+    v0 = va0[1] - va0[0]
     print(p0)
-    ax.quiver(p0[0][0], p0[0][1], p0[0][2], v0[0], v0[1], v0[2], length=0.5, normalize=False, color="green",linewidth=2)
-    aux =points[:].transpose(1,0,2)
-    p1,va1 = deCasteljauSurface(t, s , aux)
+    ax.quiver(p0[0][0], p0[0][1], p0[0][2], v0[0], v0[1], v0[2], length=0.5, normalize=False, color="green",
+              linewidth=2)
+    aux = points[:].transpose(1, 0, 2)
+    p1, va1 = deCasteljauSurface(t, s, aux)
     v1 = va1[1] - va1[0]
     ax.quiver(p1[0][0], p1[0][1], p1[0][2], v1[0], v1[1], v1[2], length=0.5, normalize=False, color="red", linewidth=2)
     v2 = np.cross(v0, v1)
     ax.quiver(p1[0][0], p1[0][1], p1[0][2], v2[0], v2[1], v2[2], length=0.5, normalize=True, color="blue", linewidth=2)
+
+
 """def drawVectors(points,t,k):
     print(deCasteljauCurves(points,t))
     print(deCasteljauCurves(points.transpose(),k))"""
 points = np.array([np.array([[1, 1, 1], [1, 2, 0], [1, 4, 1]]),
-     np.array([[2.5, 1, 2], [2.5, 3, 4], [2.5, 4, 2]]),
-     np.array([[4, 1, 0], [4, 2, 1], [4, 4, 0]])])
-#deCasteljauSurface(0.5, 0.5, points)
+                   np.array([[2.5, 1, 2], [2.5, 3, 4], [2.5, 4, 2]]),
+                   np.array([[4, 1, 0], [4, 2, 1], [4, 4, 0]])])
+# deCasteljauSurface(0.5, 0.5, points)
 
 ax = fig.add_subplot(1, 1, 1, projection='3d')
 
-
 grid = createBezierSurface(points, 10, 10)
-drawPoints(ax,points)
+drawPoints(ax, points)
 drawSurface(grid)
 drawTriangles(ax)
-drawVectorsPoint(points, 0.5, 0.5,ax)
-
-
-
+drawVectorsPoint(points, 0.5, 0.5, ax)
 
 plt.show()
